c393651c6a00b07438074563dccce48a91b19cf2
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-03-18 21:41:28 +0900

    ng generate top component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormComponent } from './containers/form/form.component';
 import { MemoListComponent } from './containers/memo-list/memo-list.component';
+import { TopComponent } from './pages/top/top.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    MemoListComponent
+    MemoListComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
