25e0bfc82211d318f8d6622e37b844645be3d75d
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-03-19 17:55:28 +0900

    define Memo interface

--- src/app/in-mem/in-memory-data.service.ts
+++ src/app/in-mem/in-memory-data.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Memo } from './../models/memo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InMemoryDataService {
   createDb() {
-    const memos = [{ id: '1', content: 'hello' }];
+    const memos: Memo[] = [{ id: '1', content: 'hello' }];
 
     return { memos };
   }
