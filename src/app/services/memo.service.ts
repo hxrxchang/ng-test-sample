91f0cb4c5258a87264eb47e659d6d8e83c98322d
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-03-20 00:54:02 +0900

    define getMemo() method

--- src/app/services/memo.service.ts
+++ src/app/services/memo.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Memo } from './../models/memo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemoService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getMemoList() {
+    return this.http.get<Memo[]>('api/memos');
+  }
 }
