631d9fd2f78eac2b0d8fdc9c437af480d4f1b73b
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-03-24 18:04:00 +0900

    componentのイベント検知

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -10,8 +10,14 @@ export class ButtonComponent implements OnInit {
   content: string;
   @Input()
   disabled: boolean;
+  @Output()
+  clickButton = new EventEmitter();
 
   constructor() {}
 
   ngOnInit() {}
+
+  onClickButton() {
+    this.clickButton.emit();
+  }
 }
--- src/app/containers/upload/upload.component.ts
+++ src/app/containers/upload/upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MemoService } from './../../services/memo.service';
 
 @Component({
   selector: 'app-upload',
@@ -11,7 +12,14 @@ export class UploadComponent implements OnInit {
     memoContent: ['', Validators.required]
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private memoService: MemoService) {}
 
   ngOnInit() {}
+
+  upload() {
+    const memoContent = this.uploadFromGroup.value.memoContent;
+    this.memoService
+      .addMemo({ id: '111', content: memoContent })
+      .subscribe(data => console.log(data));
+  }
 }
