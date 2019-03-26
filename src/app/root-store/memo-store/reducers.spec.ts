715c4ba979a4bae706ffa12e8e4d9be50e7b2ab7
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-03-26 20:02:09 +0900

    memo reducerのテストを100%に

---
+++ src/app/root-store/memo-store/reducers.spec.ts
@@ -0,0 +1,41 @@
+import {
+  FetchMemoListRequestAction,
+  FetchMemoListSuccessAction,
+  AddMemoRequestAction,
+  AddMemoSuccessAction
+} from './actions';
+import { memoReducer } from './reducers';
+import { State } from './state';
+
+describe('memoReducer', () => {
+  const state: State = {
+    memoList: []
+  };
+
+  it('FetchMemoListRequestAction', () => {
+    expect(memoReducer(state, new FetchMemoListRequestAction())).toEqual(state);
+  });
+
+  it('FetchMemoListSuccessAction', () => {
+    const memoList = [
+      { id: '1', content: 'hello' },
+      { id: '2', content: 'goobye' }
+    ];
+    expect(
+      memoReducer(state, new FetchMemoListSuccessAction(memoList))
+    ).toEqual({ memoList });
+  });
+
+  it('AddMemoRequestAction', () => {
+    const memo = { id: '1', content: 'hello' };
+    expect(memoReducer(state, new AddMemoRequestAction(memo))).toEqual(state);
+  });
+
+  it('AddMemoSuccessAction', () => {
+    const memo = { id: '1', content: 'hello' };
+    expect(memoReducer(state, new AddMemoSuccessAction(memo))).toEqual({
+      ...state,
+      memoList: [...state.memoList, memo]
+    });
+  });
+});
