fe337c3fd7296c80b08363bd58ac3a125d516f6c
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-03-26 20:19:27 +0900

    memo selectorのテスト

---
+++ src/app/root-store/memo-store/selectors.spec.ts
@@ -0,0 +1,13 @@
+import { selectMemoList } from './selectors';
+import { State } from './state';
+
+describe('memoSelector', () => {
+  const initialState: State = {
+    memoList: [{ id: '1', content: 'hello' }, { id: '2', content: 'goodbye' }]
+  };
+  it('selectMemoList', () => {
+    expect(selectMemoList.projector(initialState)).toEqual(
+      initialState.memoList
+    );
+  });
+});
